9a5c8ff1f699ffc2f3ce223caac3b970f4b4a83b
guanrenyang <renyangguan@outlook.com>
2024-05-28 16:08:05 +0800

    API of PE and VectorRegister

--- PE.h
+++ PE.h
@@ -3,11 +3,47 @@
 
 #include "common.h"
 #include "CodeBlock.h"
+#include "Scheduler.h"
+
+class VectorRegister;
 
 class PE {
 public:
-    // TODO: Wrap the following queues into a codeBlocks pool
-    std::vector<std::shared_ptr<CodeBlock>> ready_CodeBlocks;
-    std::vector<std::shared_ptr<CodeBlock>> waiting_CodeBlocks;
+    PE() {
+        scheduler = std::make_shared<Scheduler>();
+        reg = std::vector<VectorRegister>(2048);
+    }
+
+    void execute_cycle(); // perform the operations in the current cycle
+    /* Fetch the next instruction from the scheduler
+     * execute the instruction */
+
+private:
+    std::shared_ptr<Scheduler> scheduler;
+
+    /* Hardware resources */
+    std::vector<VectorRegister> reg;
 };
+
+/* 1024 bit vector register */
+class VectorRegister {
+public:
+    VectorRegister() {
+        data = std::vector<uint8_t>(128, 0);
+    }
+
+    uint8_t operator[](const int i) const { return data[i]; }
+    uint8_t& operator[](const int i) { return data[i]; }
+
+    void write_reg(const std::vector<uint8_t>& new_data) {
+        data = new_data;
+    }
+    std::vector<uint8_t> read_reg() const {
+        return data;
+    }
+
+private:
+    std::vector<uint8_t> data;
+};
+
 #endif
