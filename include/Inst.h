72232321ade4cd034f43ca4aa82c8261d6424932
Renyang Guan <695401454@qq.com>
2024-06-22 07:50:42 +0000

    add copy inst for testing

--- include/Inst.h
+++ include/Inst.h
@@ -54,13 +54,14 @@ public:
 };
 
 class CopyInst final: public Inst {
+public:
     int src_pe_row;
     int src_pe_col;
     int dst_pe_row;
     int dst_pe_col;
     int src_reg_idx;
     int dst_reg_idx;
-    CopyInst(int src_pe_row, int src_pe_col, int dst_pe_row, int dst_pe_col, int reg_idx) : src_pe_row(src_pe_row), src_pe_col(src_pe_col), dst_pe_row(dst_pe_row), dst_pe_col(dst_pe_col), dst_reg_idx(reg_idx) {}
+    CopyInst(int src_pe_row, int src_pe_col, int src_reg_idx, int dst_pe_row, int dst_pe_col, int dst_reg_idx) : src_pe_row(src_pe_row), src_pe_col(src_pe_col), src_reg_idx(src_reg_idx), dst_pe_row(dst_pe_row), dst_pe_col(dst_pe_col), dst_reg_idx(dst_reg_idx) {}
     void execute(VectorRegisterFile &reg, const std::shared_ptr<SPM>& memory, const std::shared_ptr<Router>& router);
 };
 class NopInst final: public Inst {
--- main.cc
+++ main.cc
@@ -1,25 +1,22 @@
-#include <algorithm>
-
-#include "include/common.h"
-#include "include/PE.h"
-#include "include/SPM.h"
-
+#include "Inst.h"
 #include "include/PEArray.h"
+#include <memory>
 
 int main() {
   // Front-end: Define a dataflow graph
   std::shared_ptr<Inst> test_inst_0 = std::make_shared<CalInst>(0, 2, 0, 1);
+  std::shared_ptr<Inst> test_copy = std::make_shared<CopyInst>(0, 0, 2, 0, 1, 0);
   std::shared_ptr<CodeBlock> code_block_0 = std::make_shared<CodeBlock>();
   code_block_0->append_instruction(test_inst_0);
+  code_block_0->append_instruction(test_copy);  
 
-  std::shared_ptr<Inst> test_inst_1 = std::make_shared<CalInst>(0, 4, 2, 3);
+  std::shared_ptr<Inst> test_inst_1 = std::make_shared<CalInst>(0, 2, 0, 1);
   std::shared_ptr<CodeBlock> code_block_1 = std::make_shared<CodeBlock>();
   code_block_1->append_instruction(test_inst_1);
 
   code_block_0->connect_to(code_block_1);
 
   // Back-end: PE excuting the dataflow graph
-
   PEArray pe_array(4, 4, 6*1024*1024);
   pe_array.add_CodeBlock(0, 0, code_block_0);
   pe_array.add_CodeBlock(0, 1, code_block_1);
@@ -32,7 +29,8 @@ int main() {
       // show result
       std::cout << "After cycle " << i << " :\n";
       pe_array.display_reg(0, 0, 2);
-      pe_array.display_reg(0, 1, 4);
+      pe_array.display_reg(0, 1, 0);
+      pe_array.display_reg(0, 1, 2);
       std::cout << std::endl;
 
       // TODO: add a CodeBlock Manager to manage all the CodeBlocks scattered on the chip
