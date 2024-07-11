99a43a2aa52f58edea0d0a34ff0a83b27155c89e
guanrenyang <renyang@isrc.iscas.ac.cn>
2024-07-11 18:47:01 +0800

    add a DataFlowGraph manager class to manage the DFG; The APIs are not mature

--- src/DataFlowGraph.cpp
+++ src/DataFlowGraph.cpp
@@ -12,3 +12,44 @@ void DataFlowGraph::connectCodeBlock(std::shared_ptr<CodeBlock> src, std::shared
     src->connect_to(dst);
 }
 
+void DataFlowGraph::appendCal(std::shared_ptr<CodeBlock> code_block, int opcode, int dst, int src0, int src1) {
+    std::shared_ptr<Inst> cal_inst = std::make_shared<CalInst>(opcode, dst, src0, src1);
+    code_block->append_instruction(cal_inst);
+    Inst_list.push_back(cal_inst);
+}
+
+void DataFlowGraph::appendLoad(std::shared_ptr<CodeBlock> code_block, int dst_pe_row, int dst_pe_col, int dst_reg_idx, int spm_addr) {
+    std::shared_ptr<Inst> ld_inst = std::make_shared<LdInst>(dst_pe_row, dst_pe_col, dst_reg_idx, spm_addr);
+    code_block->append_instruction(ld_inst);
+    Inst_list.push_back(ld_inst);
+}
+
+void DataFlowGraph::appendStore(std::shared_ptr<CodeBlock> code_block, int src_pe_row, int src_pe_col, int src_reg_idx, int spm_addr) {
+    std::shared_ptr<Inst> st_inst = std::make_shared<StInst>(src_pe_row, src_pe_col, src_reg_idx, spm_addr);
+    code_block->append_instruction(st_inst);
+    Inst_list.push_back(st_inst);
+}
+
+void DataFlowGraph::appendCopy(std::shared_ptr<CodeBlock> code_block, int src_pe_row, int src_pe_col, int src_reg_idx, int dst_pe_row, int dst_pe_col, int dst_reg_idx) {
+    std::shared_ptr<Inst> copy_inst = std::make_shared<CopyInst>(src_pe_row, src_pe_col, src_reg_idx, dst_pe_row, dst_pe_col, dst_reg_idx);
+    code_block->append_instruction(copy_inst);
+    Inst_list.push_back(copy_inst);
+}
+
+void DataFlowGraph::signalDownstream() {
+    for (const auto& inst : Inst_list) {
+        inst->signal_downstream_if_finished();
+    }
+    for (const auto& CodeBlock : CodeBlock_list) {
+        CodeBlock->signal_downstream_if_finished();
+    }
+}
+
+void DataFlowGraph::updateConstraint() {
+    for (const auto& inst : Inst_list) {
+        inst->update_constraint();
+    }
+    for (const auto& CodeBlock : CodeBlock_list) {
+        CodeBlock->update_constraint();
+    }
+}
--- test/test_parallel_codeblock.cpp
+++ test/test_parallel_codeblock.cpp
@@ -1,48 +1,28 @@
 #include "../include/Inst.h"
 #include "../include/PEArray.h"
-#include <memory>
-static int use_copy(){
-    std::list<std::shared_ptr<Inst>> inst_list;
-    std::list<std::shared_ptr<CodeBlock>> code_block_list;
+#include "../include/DataFlowGraph.h"
 
-    std::shared_ptr<CodeBlock> code_block_0 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_0);
-    std::shared_ptr<CodeBlock> code_block_1 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_1);
-    std::shared_ptr<CodeBlock> code_block_2 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_2);
+static int use_copy(){
+    DataFlowGraph dfg;
+
+    std::shared_ptr<CodeBlock> code_block_0 = dfg.createCodeBlock();
+    std::shared_ptr<CodeBlock> code_block_1 = dfg.createCodeBlock();
+    std::shared_ptr<CodeBlock> code_block_2 = dfg.createCodeBlock();
 
     // code block 0
-    std::shared_ptr<Inst> add_0 = std::make_shared<CalInst>(0, 0, 0, 1);
-    std::shared_ptr<Inst> copy_0 = std::make_shared<CopyInst>(0, 0, 0, 0, 1, 0);
-    code_block_0->append_instruction(add_0);
-    code_block_0->append_instruction(copy_0);
-    inst_list.push_back(add_0);
-    inst_list.push_back(copy_0);
-    add_0->connect_to(copy_0);
+    dfg.appendCal(code_block_0, 0, 0, 0, 1);
+    dfg.appendCopy(code_block_0, 0, 0, 0, 0, 1, 0);
 
     // code block 1
-    std::shared_ptr<Inst> add_1 = std::make_shared<CalInst>(0, 2, 2, 3);
-    std::shared_ptr<Inst> copy_1 = std::make_shared<CopyInst>(0, 0, 2, 0, 1, 1);
-    code_block_1->append_instruction(add_1);
-    code_block_1->append_instruction(copy_1);
-    inst_list.push_back(add_1);
-    inst_list.push_back(copy_1);
-    add_1->connect_to(copy_1);
+    dfg.appendCal(code_block_1, 0, 2, 2, 3);
+    dfg.appendCopy(code_block_1, 0, 0, 2, 0, 1, 1);
 
     // codeblock 2
-    std::shared_ptr<Inst> add_2 = std::make_shared<CalInst>(0, 0, 0, 1);
-    std::shared_ptr<Inst> store_2 = std::make_shared<StInst>(0, 1, 0,  256);
-    code_block_2->append_instruction(add_2);
-    code_block_2->append_instruction(store_2);
-    inst_list.push_back(add_2);
-    inst_list.push_back(store_2);
-    add_2->connect_to(store_2);
+    dfg.appendCal(code_block_2, 0, 0, 0, 1);
+    dfg.appendStore(code_block_2, 0, 1, 0, 256);
 
-
-    code_block_0->connect_to(code_block_2);
-    code_block_1->connect_to(code_block_2);
-
+    dfg.connectCodeBlock(code_block_0, code_block_2);
+    dfg.connectCodeBlock(code_block_1, code_block_2);
 
     PEArray pe_array(4, 4, 6*1024*1024);
     pe_array.add_CodeBlock(0, 0, code_block_0);
@@ -79,76 +59,33 @@ static int use_copy(){
         pe_array.display_spm(256);
         std::cout << std::endl;
 
-        // TODO: add a CodeBlock Manager to manage all the CodeBlocks scattered on the chip
-        // update the constraints of each code block
-        // code_block_0->signal_downstream_if_finished();
-        // code_block_1->signal_downstream_if_finished();
-
-        for (const auto& code_block: code_block_list) {
-            code_block->signal_downstream_if_finished();
-        }
-        for (const auto& code_block: code_block_list) {
-            code_block->update_constraint();
-        }
-        for (const auto& inst : inst_list) {
-            inst->signal_downstream_if_finished();
-        }
-        for (const auto& inst : inst_list) {
-            inst->update_constraint();
-        }
+        dfg.signal_and_update();
     }
     return 0;
 }
 static int use_ldst(){
-    std::list<std::shared_ptr<Inst>> inst_list;
-    std::list<std::shared_ptr<CodeBlock>> code_block_list;
+    DataFlowGraph dfg;
 
-    std::shared_ptr<CodeBlock> code_block_0 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_0);
-    std::shared_ptr<CodeBlock> code_block_1 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_1);
-    std::shared_ptr<CodeBlock> code_block_2 = std::make_shared<CodeBlock>();
-    code_block_list.push_back(code_block_2);
+    std::shared_ptr<CodeBlock> code_block_0 = dfg.createCodeBlock();
+    std::shared_ptr<CodeBlock> code_block_1 = dfg.createCodeBlock();
+    std::shared_ptr<CodeBlock> code_block_2 = dfg.createCodeBlock();
 
     // code block 0
-    std::shared_ptr<Inst> add_0 = std::make_shared<CalInst>(0, 0, 0, 1);
-    std::shared_ptr<Inst> store_0 = std::make_shared<StInst>(0, 0, 0, 0);
-    code_block_0->append_instruction(add_0);
-    code_block_0->append_instruction(store_0);
-    inst_list.push_back(add_0);
-    inst_list.push_back(store_0);
-    add_0->connect_to(store_0);
+    dfg.appendCal(code_block_0, 0, 0, 0, 1);
+    dfg.appendStore(code_block_0, 0, 0, 0, 0);
 
     // code block 1
-    std::shared_ptr<Inst> add_1 = std::make_shared<CalInst>(0, 2, 2, 3);
-    std::shared_ptr<Inst> store_1 = std::make_shared<StInst>(0, 0, 2,  128);
-    code_block_1->append_instruction(add_1);
-    code_block_1->append_instruction(store_1);
-    inst_list.push_back(add_1);
-    inst_list.push_back(store_1);
-    add_1->connect_to(store_1);
+    dfg.appendCal(code_block_1, 0, 2, 2, 3);
+    dfg.appendStore(code_block_1, 0, 0, 2, 128);
 
     // codeblock 2
-    std::shared_ptr<Inst> ld_2_0 = std::make_shared<LdInst>(0, 1, 0,  0);
-    std::shared_ptr<Inst> ld_2_1 = std::make_shared<LdInst>(0, 1, 1,  128);
-    std::shared_ptr<Inst> add_2 = std::make_shared<CalInst>(0, 0, 0, 1);
-    std::shared_ptr<Inst> store_2 = std::make_shared<StInst>(0, 1, 0,  256);
-    code_block_2->append_instruction(ld_2_0);
-    code_block_2->append_instruction(ld_2_1);
-    code_block_2->append_instruction(add_2);
-    code_block_2->append_instruction(store_2);
-    inst_list.push_back(ld_2_0);
-    inst_list.push_back(ld_2_1);
-    inst_list.push_back(add_2);
-    inst_list.push_back(store_2);
-    ld_2_0->connect_to(ld_2_1);
-    ld_2_1->connect_to(add_2);
-    add_2->connect_to(store_2);
+    dfg.appendLoad(code_block_2, 0, 1, 0, 0);
+    dfg.appendLoad(code_block_2, 0, 1, 1, 128);
+    dfg.appendCal(code_block_2, 0, 0, 0, 1);
+    dfg.appendStore(code_block_2, 0, 1, 0, 256);
 
-
-    code_block_0->connect_to(code_block_2);
-    code_block_1->connect_to(code_block_2);
-
+    dfg.connectCodeBlock(code_block_0, code_block_2);
+    dfg.connectCodeBlock(code_block_1, code_block_2);
 
     PEArray pe_array(4, 4, 6*1024*1024);
     pe_array.add_CodeBlock(0, 0, code_block_0);
@@ -190,21 +127,10 @@ static int use_ldst(){
         // code_block_0->signal_downstream_if_finished();
         // code_block_1->signal_downstream_if_finished();
 
-        for (const auto& code_block: code_block_list) {
-            code_block->signal_downstream_if_finished();
-        }
-        for (const auto& code_block: code_block_list) {
-            code_block->update_constraint();
-        }
-        for (const auto& inst : inst_list) {
-            inst->signal_downstream_if_finished();
-        }
-        for (const auto& inst : inst_list) {
-            inst->update_constraint();
-        }
+        dfg.signal_and_update();
     }
     return 0;
 }
 int main() {
-    return use_ldst();
+    return use_copy();
 }
